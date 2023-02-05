5f6c59e995ab17b292a71e69bee821a1ca174fec
Jose Alvarado <devjosealvarado@gmail.com>
2023-02-05 16:54:54 -0400

    Optimizada función de crear ordenes

--- views/mesero/index.js
+++ views/mesero/index.js
@@ -133,7 +133,7 @@ const getMenu = async () => {
                     </li>
                 `;
 
-                btnDeleted.style.display = '';
+                // btnDeleted.style.display = '';
                 formBtn.style.display = '';
                 mesa.style.display = '';
                 ordenPreliminar.append(PreviewOrden)
@@ -250,220 +250,40 @@ const getOrdenes = async () => {
 
     try {
         const { data } = await axios.get('/api/ordenes', newOrden);
-    // console.log(data);
+        console.log(data);
 
-    // const busqueda = data.reduce((acc, item) => {
-    //     acc[item.mesa] = ++acc[item.mesa] || 0;
-    //     return acc;
-    // }, {});
+        data.forEach(orden => {
+            console.log(orden.mesa);
+            const mesa = orden.mesa;
+            console.log(orden.precio);
+            const monto = orden.precio;
+            console.log(orden.date);
+            const fecha = orden.date;
+            console.log(orden.time);
+            const hora = orden.time;
+            console.log(orden.orden);
+            const platos = orden.orden
+            console.log(orden.status);
+            const status = orden.status;
 
-    
-    // const duplicados = data.filter( (item) => {
-    //     return busqueda[item.mesa]
-        
-    // });
-    // console.log(duplicados);
-
-    const z = () => {
-        for (let index = 0; index < data.length; index++) {
-            const element = data[index].mesa;
-            const orden = data[index];
-            const posibleLi = document.createElement('li');
-            const btnDeletedA = document.createElement('button')
-            btnDeletedA.setAttribute('class', 'btn-deleted');
-            btnDeletedA.innerHTML = '✖'
-            // console.log(btnDeletedA);
-            if (element === 5) {
-                contentOrdenes5.style.display = '';
-                // console.log(orden.mesa);
-                console.log(orden.orden);
-                const plato = orden.orden;
-                const price = orden.precio
-                const amount = orden.cantidad;
-                console.log(price);
-                console.log(amount);
-
-                // const e = parseInt(amount, 10)
-                // const w = parseInt(price, 10);
-                const total = price*amount;
-                console.log(total);
-                // const total = price + amount;
-                // console.log(total);
-
-                mesa5.innerHTML = `Mesa: ${orden.mesa}`;
-                fecha5.innerHTML = `Fecha: ${orden.date}`;
-                hora5.innerHTML = `Hora: ${orden.time}`;
-                posibleLi.innerHTML = `
-                <li class="orden-item" id="${orden.id}">
-                    <span>${plato} x $${total}</span>
+            const plato = document.createElement('div')
+            plato.innerHTML = `
+                <li class="orden-item">
+                    <p>Mesa: ${mesa}</p>
+                    <p>Monto: $${monto}</p>
+                    <p>Fecha: ${fecha}</p>
+                    <p>Hora: ${hora}</p>
+                    <p>${platos}</p>
+                    <p>Estado: ${status}</p>
                 </li>
+            `;
 
-                `;
-                
-                contentOrdenes5.append(posibleLi);
-                
-            }
-
-            let totalNumeroA=0;
-            const dataContentOrdenes5 = contentOrdenes5.children
-            for (let index = 5; index < dataContentOrdenes5.length; index++) {
-                const items = dataContentOrdenes5[index].children[0].children[0].innerHTML;
-                console.log(items);
-                const itemsTotal = items.split(' ');
-                const cantidadItem = itemsTotal[0];
-                const itemsTotalReverse = itemsTotal.reverse();
-                const itemsUnique = itemsTotalReverse[0];
-                const itemsArray = itemsUnique.split('$');
-                // const itemsOnlyN = itemsArray.shift();
-                console.log(itemsArray[1]);
-                let h = parseInt(cantidadItem, 10)
-                let c = parseInt(itemsArray[1], 10)
-                parseInt(totalNumeroA, 10)
-                let montoFinal = h*c;
-                
-                totalNumeroA += montoFinal;
-                console.log(totalNumeroA);
-                montoTotal5.innerHTML = `Monto: $${totalNumeroA}`
-                
-            }
-
-            // ----------------------------------------------------
-
-            if (element === 6) {
-                // console.log(orden.mesa);
-                contentOrdenes6.style.display = '';
-                // console.log(orden.orden);
-                const plato = orden.orden;
-                const price = orden.precio
-                const amount = orden.cantidad;
-                // console.log(price);
-                // console.log(amount);
-
-                // const e = parseInt(amount, 10)
-                // const w = parseInt(price, 10);
-                const total = price*amount;
-                // console.log(total);
-                // const total = price + amount;
-                // console.log(total);
-
-                mesa6.innerHTML = `Mesa: ${orden.mesa}`;
-                fecha6.innerHTML = `Fecha: ${orden.date}`;
-                hora6.innerHTML = `Hora: ${orden.time}`;
-                posibleLi.innerHTML = `
-                <li class="orden-item" id="${orden.id}">
-                    <span>${plato} x $${total}</span>
-                </li>
-
-                `;
-
-                contentOrdenes6.append(posibleLi);
-
-            }
-
-            let totalNumeroB=0;
-            const dataContentOrdenes6 = contentOrdenes6.children
-            for (let index = 5; index < dataContentOrdenes6.length; index++) {
-                const items = dataContentOrdenes6[index].children[0].children[0].innerHTML;
-                // console.log(items);
-                const itemsTotal = items.split(' ');
-                const cantidadItem = itemsTotal[0];
-                const itemsTotalReverse = itemsTotal.reverse();
-                const itemsUnique = itemsTotalReverse[0];
-                const itemsArray = itemsUnique.split('$');
-                // const itemsOnlyN = itemsArray.shift();
-                // console.log(itemsArray[1]);
-                let h = parseInt(cantidadItem, 10)
-                let c = parseInt(itemsArray[1], 10)
-                parseInt(totalNumeroA, 10)
-                let montoFinal = h*c;
-                
-                totalNumeroB += montoFinal;
-                // console.log(totalNumeroB);
-                montoTotal6.innerHTML = `Monto: $${totalNumeroB}`
-                
-            }
-        }
-    }
-
-    z();
-    
-
-    
-
-    // data.forEach(a => {
-
-    //     // if (a.mesa === 9) {
-    //     //     if (a.status === 'Pendiente') {
-    //     //         console.log(a);;
-    //     //     }
-    //     // }
-    // })
-
-//         data.forEach(ordenItem => {
-//             const ordenLi = document.createElement('li');
-//             // console.log(ordenLi);
-//             if (ordenItem.status === 'Pendiente') {
-//                 ordenLi.innerHTML = `
-//                 <li class="orden-item" id="${ordenItem.id}">
-// 				<p>Mesa: </p>
-//                 <p>${ordenItem.mesa}</p>
-//                 <p>Orden: </p>
-// 				<p>${ordenItem.orden}</p>
-//                 <p class="fecha">Fecha: ${ordenItem.date}</p>
-//                 <p class="hora">Hora: ${ordenItem.time}</p>
-//                 <p>Estado: </p>
-//                 <p class="estado">${ordenItem.status}</p>
-// 				<button class="btn-edit">✎</button>
-// 				<button class="btn-deleted">✖</button>
-//                 <div id="content-cantidad">
-//                 <button class="disminuir">-</button>
-//                 <span name="" class="cantidad">0</span>
-//                 <button class="incrementar">+</button>
-//                 </div>
-// 			</li>
-//             `;
-
-//             let a = 0;
-
-//             const btnDownLi = ordenLi.children[0].children[10].children[0];
-//                 const monto = ordenLi.children[0].children[10].children[1];
-//             btnDownLi.addEventListener('click', () => {
-//         a--;
-//     // a = (a < 10) ? "0" + a : a;
-//         monto.innerText = a;
-//         console.log(a);
-
-// });
-
-//             contentOrdenes.append(ordenLi)
-//             }
-
-//             if (ordenItem.status === 'Completado') {
-//                 ordenLi.innerHTML = `
-//                 <li class="orden-item" id="${ordenItem.id}">
-// 				<p>Mesa: </p>
-//                 <p>${ordenItem.mesa}</p>
-//                 <p>Orden: </p>
-// 				<p>${ordenItem.orden}</p>
-//                 <p class="fecha">Fecha: ${ordenItem.date}</p>
-//                 <p class="hora">Hora: ${ordenItem.time}</p>
-//                 <p>Estado: </p>
-//                 <p class="estado">${ordenItem.status}</p>
-// 				<button class="btn-edit">✎</button>
-// 				<button class="btn-deleted">✖</button>
-//                 <div id="content-cantidad">
-//                 <button class="disminuir">-</button>
-//                 <span name="" class="cantidad">0</span>
-//                 <button class="incrementar">+</button>
-//             </div>
-// 			</li>
-//             `;
+            contentOrdenes.append(plato);
+        })
 
 
-//             contentCompletados.append(ordenLi)
-//             }
-//         });
 
+   
 
     } finally {}
 };
@@ -472,61 +292,61 @@ getOrdenes();
 // <p class="estado">${e.target.parentElement.children[6].innerHTML}</p>
 // EVENTO PARA EDITAR COMIDAS
 
-contentOrdenes.addEventListener('click', async e => {
-    if (e.target.classList.contains('btn-edit')) {
-        const id = e.target.parentElement.id;
-		const ordenItem = e.target.parentElement;
-        // console.log(e.target.parentElement);
-        ordenItem.innerHTML = `
-        <p>Mesa: </p>
-        <p class="mesa-edit">${e.target.parentElement.children[1].innerHTML}</p>
-        <p>Orden: </p>
-        <p class="orden-edit">${e.target.parentElement.children[3].innerHTML}</p>
-        <p class="fecha">${e.target.parentElement.children[4].innerHTML}</p>
-        <p class="hora">${e.target.parentElement.children[5].innerHTML}</p>
-        <p>Estado: </p>
-        <select name="" id="" class="status">
-                <option value="Pendiente">Pendiente</option>
-                <option value="Completado">Completado</option>
-            </select>
-        <button class="btn-editing">✔</button>
-        `;
-    } else if (e.target.classList.contains('btn-editing')) {
-        const id = e.target.parentElement.id;
-		const ordenItem = e.target.parentElement;
-        const plato = document.querySelector('.meal-edit');
-		const price = document.querySelector('.price-edit');
+// contentOrdenes.addEventListener('click', async e => {
+//     if (e.target.classList.contains('btn-edit')) {
+//         const id = e.target.parentElement.id;
+// 		const ordenItem = e.target.parentElement;
+//         // console.log(e.target.parentElement);
+//         ordenItem.innerHTML = `
+//         <p>Mesa: </p>
+//         <p class="mesa-edit">${e.target.parentElement.children[1].innerHTML}</p>
+//         <p>Orden: </p>
+//         <p class="orden-edit">${e.target.parentElement.children[3].innerHTML}</p>
+//         <p class="fecha">${e.target.parentElement.children[4].innerHTML}</p>
+//         <p class="hora">${e.target.parentElement.children[5].innerHTML}</p>
+//         <p>Estado: </p>
+//         <select name="" id="" class="status">
+//                 <option value="Pendiente">Pendiente</option>
+//                 <option value="Completado">Completado</option>
+//             </select>
+//         <button class="btn-editing">✔</button>
+//         `;
+//     } else if (e.target.classList.contains('btn-editing')) {
+//         const id = e.target.parentElement.id;
+// 		const ordenItem = e.target.parentElement;
+//         const plato = document.querySelector('.meal-edit');
+// 		const price = document.querySelector('.price-edit');
 
-        const mesa = document.querySelector('.mesa-edit');
-        const orden = document.querySelector('.orden-edit');
-        const estado = document.querySelector('.status')
-        // let estado = false;
+//         const mesa = document.querySelector('.mesa-edit');
+//         const orden = document.querySelector('.orden-edit');
+//         const estado = document.querySelector('.status')
+//         // let estado = false;
 
-        // console.log(estado);
-        // console.log(mesa.value);
-        // console.log(orden);
-        // console.log(estado);
+//         // console.log(estado);
+//         // console.log(mesa.value);
+//         // console.log(orden);
+//         // console.log(estado);
 
-        // console.log(hora);
-        let PRICE_REGEX =  /^[0-9]/;
-        let isValid = PRICE_REGEX.test(mesa.innerHTML);
-        if (isValid == true) {
-            ordenItem.innerHTML = `
-            <p>Mesa: </p>
-            <p>${mesa.innerHTML}</p>
-            <p>Orden: </p>
-            <p>${orden.innerHTML}</p>
-            <p class="fecha">${e.target.parentElement.children[4].innerHTML}</p>
-            <p class="hora">${e.target.parentElement.children[5].innerHTML}</p>
-            <p>Estado: </p>
-            <p class="estado">${estado.value}</p>
-            <button class="btn-edit">✎</button>
-            <button class="btn-deleted">✖</button>
-            `;
-            await axios.patch(`/api/ordenes/${id}`, {mesa: mesa.innerHTML, orden: orden.innerHTML, status: estado.value});
-        }
-    }
-});
+//         // console.log(hora);
+//         let PRICE_REGEX =  /^[0-9]/;
+//         let isValid = PRICE_REGEX.test(mesa.innerHTML);
+//         if (isValid == true) {
+//             ordenItem.innerHTML = `
+//             <p>Mesa: </p>
+//             <p>${mesa.innerHTML}</p>
+//             <p>Orden: </p>
+//             <p>${orden.innerHTML}</p>
+//             <p class="fecha">${e.target.parentElement.children[4].innerHTML}</p>
+//             <p class="hora">${e.target.parentElement.children[5].innerHTML}</p>
+//             <p>Estado: </p>
+//             <p class="estado">${estado.value}</p>
+//             <button class="btn-edit">✎</button>
+//             <button class="btn-deleted">✖</button>
+//             `;
+//             await axios.patch(`/api/ordenes/${id}`, {mesa: mesa.innerHTML, orden: orden.innerHTML, status: estado.value});
+//         }
+//     }
+// });
 
 // --------------- EVENTO PARA ELIMINAR COMIDAS -----------------
 
@@ -548,42 +368,6 @@ ordenPreliminar.addEventListener('click', e => {
     }
 })
 
-contentOrdenes5.addEventListener('click', async e => {
-    if (e.target.classList.contains('btn-deleted')) {
-        const id = e.target.parentElement.id;
-            // e.target.parentElement.remove();
-        //   console.log(e.target.parentElement.children[5].children[0].id);
-        const arr = e.target.parentElement.children
-          for (let index = 5; index < arr.length; index++) {
-            const element = e.target.parentElement.children[index].children[0].id;
-            console.log(element);
-            await axios.delete(`/api/ordenes/${element}`)
-          }
-
-        // await axios.delete(`/api/ordenes/${id}`)
-    }
-});
-
-contentOrdenes6.addEventListener('click', async e => {
-    if (e.target.classList.contains('btn-deleted')) {
-        const id = e.target.parentElement.id;
-            // e.target.parentElement.remove();
-        //   console.log(e.target.parentElement.children[5].children[0].id);
-        const arr = e.target.parentElement.children
-          for (let index = 5; index < arr.length; index++) {
-            // const element = e.target.parentElement.children[index].children[0].id;
-            const element = e.target.parentElement.children[index].children[0].id;
-            console.log(element);
-            await axios.delete(`/api/ordenes/${element}`)
-          }
-
-        // await axios.delete(`/api/ordenes/${id}`)
-    }
-});
-
-
-
-// ----------- EVENTO PARA REGISTRAR ORDEN ----------------
 
 formBtn.addEventListener('click', async e=> {
     // console.log(ordenPreliminar.children);
@@ -598,7 +382,7 @@ formBtn.addEventListener('click', async e=> {
     
     let platoArray = []
     let precioArray = []
-    let mesaArray = 0;
+    
     
 
     for (let i = 4; i < data.length; i++) {
@@ -620,18 +404,6 @@ formBtn.addEventListener('click', async e=> {
         platoArray.push(plato)
         precioArray.push(multiplicacion)
         
-        // const ordenLi = document.createElement('li');
-        // ordenLi.innerHTML = `<li class="orden-item" id="">
-		// 		<p>Mesa:  </p>
-        //         <p>Orden: </p>
-		// 		<p>${plato}</p>
-               
-        //         </div>
-		// 	</li>`
-
-        // contentOrdenes.append(ordenLi);
-
-        // window.location.reload()
 
     };
     
@@ -640,29 +412,19 @@ formBtn.addEventListener('click', async e=> {
     const sumatoriaPrecio = precioArray.reduce((a,b) => a + b, 0)
     console.log(sumatoriaPrecio);
 
-    const newOrden = {
-        mesa: mesa.value,
-        orden: platoDefinito,
-        cantidad: 0,
-        precio: sumatoriaPrecio,
-        status: 'Pendiente',
-        // date: Date.now()
+    if (mesa.value) {
+        const newOrden = {
+            mesa: mesa.value,
+            orden: platoDefinito,
+            cantidad: 0,
+            precio: sumatoriaPrecio,
+            status: 'Pendiente',
+            // date: Date.now()
+        }
+        await axios.post('/api/ordenes', newOrden, { withCredentials: true});
+        window.location.reload()
+    } else {
+        alert('Especifíca el número de mesa')
     }
-    await axios.post('/api/ordenes', newOrden, { withCredentials: true});
-
-
-    // const newOrden = {
-    //     mesa: mesa.value,
-    //     orden: plato,
-    //     cantidad: cantidadPlatoNumber,
-    //     precio: precio,
-    //     status: 'Pendiente',
-    //     // date: Date.now()
-    // }
-    // await axios.post('/api/ordenes', newOrden, { withCredentials: true});
-
 })
 
-// formBtn.addEventListener('click', e => {
-//     console.log(ordenPreliminar);
-// })
